d42e3c183807a298e7074f641dbe57fc9578862f
MAHADEV0023 <mahadev636308@gmail.com>
2022-07-12 21:13:34 +0530

    22 GitCreate a project in IntelliJ Idea. Track that project using git init. Add some class files in the project. Do the Initial Commit. Then after modify some files and add some more files. check the status using git status. Add the untracked files to the staging area. Provide two more commits. Create a branch Developer. Add some class file in Development branch. Then merge Master and Development branch. Also create .gitignore file and add some file in gitignore. And finally remove the tracking of your project.

---
+++ src/greater_number.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class greater_number {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int z=sc.nextInt();
+        int x=sc.nextInt();
+        if(z>x){
+            System.out.println("z is greater");}
+        else {
+            System.out.println("x is greater");
+        }
+    }
+}
---
+++ src/mod_operator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class mod_operator {
+    public static void main(String[] args) {
+        int a=10;
+        Scanner sc =new Scanner(System.in);
+        int z=sc.nextInt();
+        int x=sc.nextInt();
+        int b=10;
+        int c=x%z;
+        System.out.println(c);
+}}
