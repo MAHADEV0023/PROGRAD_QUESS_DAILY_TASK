0500c708c4da005659a0fc79a2e5ee6cbf204027
Mahadev <mahadev636308@gmail.com>
2022-08-03 16:51:09 +0530

    Delete src/HotelManagement directory

--- src/HotelManagement/src/main/java/com/HotelManagement/HotelManagement/Repository/HotelRepo.java
+++
@@ -1,6 +0,0 @@
-package com.HotelManagement.HotelManagement.Repository;
-import com.HotelManagement.HotelManagement.Mdels.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface HotelRepo extends JpaRepository<Hotel,Integer>{
-
-}
