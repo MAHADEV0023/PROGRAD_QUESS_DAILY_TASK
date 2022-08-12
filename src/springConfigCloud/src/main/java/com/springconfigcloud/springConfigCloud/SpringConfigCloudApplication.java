c9afdbe0db60b8874c91498905481375ca799cb0
Mahadev <mahadev636308@gmail.com>
2022-08-12 13:08:39 +0530

    Add files via upload

---
+++ src/springConfigCloud/src/main/java/com/springconfigcloud/springConfigCloud/SpringConfigCloudApplication.java
@@ -0,0 +1,18 @@
+package com.springconfigcloud.springConfigCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+
+
+@SpringBootApplication
+public class SpringConfigCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigCloudApplication.class, args);
+	}
+
+}
---
+++ src/springConfigCloud/src/test/java/com/springconfigcloud/springConfigCloud/SpringConfigCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springconfigcloud.springConfigCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
